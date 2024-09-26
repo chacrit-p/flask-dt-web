b184f0f06658dd25e2f17f24d10f9f96ad1c14c0
sphinx-p <chacritpoonpipit@gmail.com>
2024-09-27 06:11:43 +0700

    install and simple setup

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from consistent import APP_DEBUG_MODE
+from flask import Flask, render_template
+from const import (
+    APP_HOST,
+    APP_DEBUG_MODE,
+    APP_PORT,
+    APP_TEMPLATE_FOLDER,
+    APP_STATIC_FOLDER,
+    APP_STATIC_URL_PATH,
+)
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=APP_TEMPLATE_FOLDER,
+    static_folder=APP_STATIC_FOLDER,
+    static_url_path=APP_STATIC_URL_PATH,
+)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=APP_DEBUG_MODE)
+    app.run(host=APP_HOST, debug=APP_DEBUG_MODE, port=APP_PORT)
